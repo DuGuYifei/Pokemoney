405b91627d4aa9b6e97e7fc83ebc6ab61c51c845
DuGuYifei <liuyifei97@163.com>
2023-08-14 23:44:41 +0800

    update sql var name and format

--- documents/databases/user-service-mysql/user-mysql.sql
+++ documents/databases/user-service-mysql/user-mysql.sql
@@ -5,10 +5,9 @@ CREATE TABLE t_users(
     `password` VARCHAR(255) NOT NULL   COMMENT '' ,
     `email` VARCHAR(50) NOT NULL   COMMENT '' ,
     `confirmation_key` VARCHAR(16)    COMMENT '' ,
-    `is_confirmation` TINYINT(1) NOT NULL   COMMENT '' ,
+    `is_confirm` TINYINT(1) NOT NULL   COMMENT '' ,
     `is_ban` TINYINT(1) NOT NULL   COMMENT '' ,
-    `register_date` DATETIME NOT NULL   COMMENT '' ,
-    `last_login_date` DATETIME    COMMENT '' ,
+    `register_date` TIMESTAMP NOT NULL   COMMENT '' ,
+    `last_login_date` TIMESTAMP    COMMENT '' ,
     PRIMARY KEY (id)
 )  COMMENT = '';
-
