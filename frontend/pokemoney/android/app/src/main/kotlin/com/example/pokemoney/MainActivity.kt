7554db7c00709a9051819a6bbe4a5c5ff3468cdb
DuGuYifei <liuyifei97@163.com>
2023-08-16 23:50:20 +0800

    POKEMON-62 Create backend branch

--- frontend/pokemoney/android/app/src/main/kotlin/com/example/pokemoney/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pokemoney
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- frontend/pokemoney/lib/widgets/barrel.dart
+++
@@ -1,6 +0,0 @@
-export 'CustomButton.dart';
-export 'CustomCheckbox.dart';
-export 'CustomClickableText.dart';
-export 'CustomHoverableListTile.dart';
-export 'CustomSquareTile.dart';
-export 'CustomTextField.dart';
