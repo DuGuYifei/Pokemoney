7855b58999f56816bdb5f4c3f4c47ded131e7d5d
DuGuYifei <liuyifei97@163.com>
2023-08-24 01:14:29 +0800

    POKEMON-71 Add snowflake service to generate snowflake id - deprecated will be replacd by leaf-meituan

---
+++ backend/snowflake-service/src/main/java/com/pokemoney/snowflakeservice/SnowflakeServiceApplication.java
@@ -0,0 +1,7 @@
+package com.pokemoney.snowflakeservice;
+
+public class SnowflakeServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
