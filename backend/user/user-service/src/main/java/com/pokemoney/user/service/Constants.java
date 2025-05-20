f344558a34560277012dfaac685f54036ea87cf7
DuGuYifei <79463006+DuGuYifei@users.noreply.github.com>
2025-05-20 17:46:50 +0200

    Update Constants.java

--- backend/user/user-service/src/main/java/com/pokemoney/user/service/Constants.java
+++ backend/user/user-service/src/main/java/com/pokemoney/user/service/Constants.java
@@ -20,14 +20,14 @@ public class Constants {
     public static String SERVICE_NAME = "user-service";
 
     /**
-     * The key of leaf-service in register-service.
+     * The key of leaf-service in register-service. No eureka any more. Use nacos instead.
      */
-    public static final String LEAF_IN_EUREKA_KEY = "leaf-service";
+    // public static final String LEAF_IN_EUREKA_KEY = "leaf-service";
 
     /**
-     * The key of redis-service in register-service.
+     * The key of redis-service in register-service. No eureka any more. Use nacos instead.
      */
-    public static final String REDIS_IN_EUREKA_KEY = "redis-service";
+    // public static final String REDIS_IN_EUREKA_KEY = "redis-service";
 
     /**
      * The key of user-service in leaf-service.
